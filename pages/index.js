2f4f035f3a2c4ac76c6f1d00ac17fccf76031d5f
ukyo-egg <ukyo161106@gmail.com>
2025-09-01 13:53:47 +0900

    owari

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,10 @@ export default function Home() {
       <section className={utilStyles.headingMd}>
         <p>hello!! I love salami!!!</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          welcome to Ukyo's site!!{' '}<br></br>
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.
         </p>
+        <h1>Blog</h1>
       </section>
     </Layout>
   );
